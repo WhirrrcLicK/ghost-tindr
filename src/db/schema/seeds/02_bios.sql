961e96f15fde49595475addf7db2b94cfaad9ec3
Kaiya Hedley <keshedley@gmail.com>
2022-09-30 17:09:28 -0600

    adding bio to schema

--- src/db/schema/seeds/02_bios.sql
+++ src/db/schema/seeds/02_bios.sql
@@ -1,6 +1,6 @@
-INSERT INTO bios (name, age, bio_image_id, location, ghost_id, looking_for, user_id, bio)
-VALUES ('insert name here', 'insert age here, no quotes', 'bio_image_id', 'insert location here', 'insert ghost_id here no quotes', 
-'insert looking_for here, no quotes', 'insert user_id here, no quotes', 'insert bio here');
+INSERT INTO bios ('Beetlejeuse', 666, bio_image_id, 'Winter River, Conneticut', ghost_id, looking_for, user_id, `Ah. Well... I attended Juilliard... I'm a graduate fo the Harvard Business school. I travel quite extensively. I lived through the Black Plague and had a pretty good time during that. I've seen THE EXORCIST ABOUT A HUNDRED AND SIXT-SEVEN TIMES, AND IT KEEPS GETTING FUNNIER EVERY SINGLE TIME I SEE IT! NOT TO MENTION THE FACT THAT YOU'RE TALKING TO A DEAD GUY! NOW WHAT DO YOU THINK? My only dealbreaker is if you say my name more than once. No matter how much I might make you want to. ; )`)
+VALUES ('Bloody Mary', 516, 'bio_image_id', 'Your bathroom mirror', ghost_id, 
+looking_for, user_id, 'insert bio here');
 
 INSERT INTO bios (name, age, bio_image_id, location, ghost_id, looking_for, user_id, bio)
 VALUES ('insert name here', 'insert age here, no quotes', 'bio_image_id', 'insert location here', 'insert ghost_id here no quotes', 
--- src/db/schema/seeds/04_ghost_types.sql
+++ src/db/schema/seeds/04_ghost_types.sql
@@ -1,5 +1,8 @@
-INSERT INTO ghost_types VALUES ('type of ghost');
-INSERT INTO ghost_types VALUES ('type of ghost');
-INSERT INTO ghost_types VALUES ('type of ghost');
-INSERT INTO ghost_types VALUES ('type of ghost');
-INSERT INTO ghost_types VALUES ('type of ghost');
+INSERT INTO ghost_types VALUES ('Poltergeist');
+INSERT INTO ghost_types VALUES ('Orb');
+INSERT INTO ghost_types VALUES ('Cold Spot');
+INSERT INTO ghost_types VALUES ('Ectoplasm');
+INSERT INTO ghost_types VALUES ('Omen');
+INSERT INTO ghost_types VALUES ('Kuchisake-onna');
+INSERT INTO ghost_types VALUES ('White Lady');
+INSERT INTO ghost_types VALUES ('Posessed Doll');
