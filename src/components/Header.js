1ebb627b46db5ff1bf0d92e110a757013ede646c
John Sweeney <jes2806@gmail.com>
2022-10-03 13:13:01 -0600

    app change

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css'
+// import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { Link, useHistory } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import IconButton from "@mui/material/IconButton";
+
+
+
+
+export default function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://i.redd.it/jb1hfi766rb61.png"
+        alt="ghost_logo"
+      />
+      <IconButton>
+        <QuestionAnswerIcon className="header__icon" fontSize="large" />
+      </IconButton>
+    </div>
+  );
+}
