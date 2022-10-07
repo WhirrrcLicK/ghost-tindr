01d7de0366ff4c1ef5adc4f3f8b1e60129760701
Jason Di <jasondisj@gmail.com>
2022-10-07 09:53:54 -0700

    Oct 7 verison

--- client/src/components/Ghost-Tindr/ChatDisplay.js
+++ client/src/components/Ghost-Tindr/ChatDisplay.js
@@ -1,16 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useCookies } from "react-cookie";
-import axios from "axios";
-import MsgInput from "./MsgInput"
-import Header from "./Header"
-import Footer from "./Footer"
-import "./conversations.scss"
+import Header from "./Header";
+import Footer from "./Footer";
+import "./conversations.scss";
 
 export default function ChatDisplay() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      name: "Ghost 1",
+      image:
+        "https://as2.ftcdn.net/v2/jpg/04/54/73/29/1000_F_454732959_pjHIUTqFz68f2VxnJMKs4JrMaXuxVo6l.jpg",
+      message: "What's up",
+    },
+    {
+      name: "Ghost 1",
+      image:
+        "https://as2.ftcdn.net/v2/jpg/04/54/73/29/1000_F_454732959_pjHIUTqFz68f2VxnJMKs4JrMaXuxVo6l.jpg",
+      message: "What's up",
+    },
+    {
+      message: "How's it going!",
+    },
+  ]);
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
+
   return (
-    <div className="">
+    <div className="chat-display">
       <Header />
-      <MsgInput />
+      <p className="chat-display-timestamp">
+        YOU MATCHED WITH ELLEN ON 10/13/22
+      </p>
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chat-display-message">
+            <img
+              className="chat-display-image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chat-display-text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chat-display-message">
+            <p className="chat-display-owntext">{message.message}</p>
+          </div>
+        )
+      )}
+      <form className="msg-form">
+        <input
+          className="msg-input"
+          type="text"
+          placeholder="Type a message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleSend} type="submit" className="send-button">
+          SEND
+        </button>
+      </form>
       <Footer />
     </div>
   );
--- client/src/components/Ghost-Tindr/Conversations.js
+++ client/src/components/Ghost-Tindr/Conversations.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import ChatDisplay from "./ChatDisplay";
-import Header from "./Header"
-import Footer from "./Footer"
-import ChatItem from "./ChatItem"
-import "./conversations.scss"
+import Header from "./Header";
+import Footer from "./Footer";
+import ChatItem from "./ChatItem";
+import "./conversations.scss";
 
 export default function Conversations() {
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   const [ghost, setGhost] = useState([]);
 
   const userId = cookies.UserId;
-  const currentGhost = ghost.find(g => userId === g.user_id)
-  console.log('current ghost:', currentGhost)
-  const matches = !currentGhost ? [] : currentGhost.matches.map(match => {
-    const foundGhost = ghost.find(g => match.user_id === g.user_id)
-    return <ChatItem ghost={foundGhost} />
-  })
+
+  const currentGhost = ghost.find((g) => userId === g.user_id);
+  const matches = !currentGhost
+    ? []
+    : currentGhost.matches.map((match, _index) => {
+        const foundGhost = ghost.find((g) => match.user_id === g.user_id);
+        return <ChatItem key={_index} ghost={foundGhost} />;
+      });
 
   const getGhost = async () => {
     try {
@@ -34,36 +36,10 @@ export default function Conversations() {
     getGhost();
   }, []);
 
-  // const updateMatches = async (matchedUserId) => {
-  //   try {
-  //     await axios.put("http://localhost:8000/addmatch", {
-  //       userId,
-  //       matchedUserId,
-  //     });
-  //     getGhost();
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // const getMatches = async () => {
-  //   try {
-  //     const response = await axios.get("http://localhost:8000/users", {
-  //       params: { userId },
-  //     });
-  //     setGhost(response.data);
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   return (
     <div className="conversations">
       <Header />
-      <div className="chat-items-container">
-      {matches}
-      </div>
+      <div className="chat-items-container">{matches}</div>
       <Footer />
     </div>
   );
